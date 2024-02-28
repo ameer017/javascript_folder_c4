f9263edbfca72819cfe1ad7e6bf5681b9ffd02f2
ameer017 <rajiabdullahi907@gmail.com>
2024-02-28 10:18:16 +0100

    new_keyword

--- OOP/script.js
+++ OOP/script.js
@@ -27,7 +27,6 @@ console.log(person.getFunction());
 console.log(person.phone_number.landline);
 
 // Using constructor:
-// Using a constructor
 function personTwo(first_name, last_name) {
   this.first_name = first_name;
   this.last_name = last_name;
@@ -39,5 +38,37 @@ let person2 = new personTwo("Zainab", "Muritala");
 console.log(person1.first_name);
 console.log(`${person2.first_name} ${person2.last_name}`);
 
+// ========================= The 'new' keyword ======================
+// The new keyword creates a new empty object
 
-// ========================= new keyword ======================
+const student = {}; //for simple object definition
+
+const eef = new Object();
+
+student.firstName = "Soliu";
+eef.firstName = "Bode";
+console.log(student);
+console.log(eef);
+
+const arr = [1, 2, 3];
+const arr1 = new Array(1, 2, 3);
+
+console.log(arr, arr1);
+
+// The basic use case of the 'new' keyword is for creating date --> date is special object built into javascript --> doesn't have any other syntax
+const myBirthDay = new Date("July 17, 2010");
+console.log(myBirthDay);
+
+// NB: whenever you create anything in javascript, you're creating a new object that is extended off of a constructor.
+
+// console.log(Array)
+// console.log(Object)
+// console.log(Number)
+// console.log(Date)
+
+const myNumber = new Number(100.0);
+console.log(myNumber.toFixed(0));
+
+const numArr = [1, 2, 3, 4]; //This is possible without the "new" keyword with literal syntax
+numArr.push(5);
+console.log(numArr);
