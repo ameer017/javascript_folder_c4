ddc5410a04c94e66225431c6d72b24074b3a23f2
ameer017 <rajiabdullahi907@gmail.com>
2024-03-26 15:38:05 +0100

    setup

--- react/color-mod/src/App.jsx
+++ react/color-mod/src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Input from "./Input";
+import Square from "./Square";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <Square />
+      <Input />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ react/color-mod/src/Input.jsx
@@ -0,0 +1,10 @@
+
+const Input = () => {
+  return (
+    <div>
+        <h2>Input</h2>
+    </div>
+  )
+}
+
+export default Input
