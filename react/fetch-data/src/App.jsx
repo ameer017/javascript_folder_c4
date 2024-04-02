004cf30e7faffc0a0beadd270577f468616e68a0
ameer017 <rajiabdullahi907@gmail.com>
2024-04-02 21:41:54 +0100

    steps_added

--- react/fetch-data/src/App.jsx
+++ react/fetch-data/src/App.jsx
@@ -3,23 +3,26 @@ import Table from "./Table";
 import Form from "./Form";
 
 function App() {
-  const API_URL = "https://jsonplaceholder.typicode.com/";
-  const [reqType, setReqType] = useState("users");
-  const [items, setItems] = useState([]);
+  const API_URL = "https://jsonplaceholder.typicode.com/"; //reqType/resourceType is gonna serve as our resource endpoint --> /posts /users /comments
+  const [reqType, setReqType] = useState("users"); //default reqType is /users
+  const [items, setItems] = useState([]); //empty array by default, response.json() will be populated into it
+
+  // useEffect normally render once and that is whenever the app loads, but we need the useEffect logic to depend on the request type so it'll take effect as we load new pages.
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await fetch(`${API_URL}${reqType}`);
-        const data = await response.json();
-        setItems(data);
+        const response = await fetch(`${API_URL}${reqType}`); //https://jsonplaceholder.typicode.com/posts || https://jsonplaceholder.typicode.com/users || https://jsonplaceholder.typicode.com/comments
+        const data = await response.json(); //convert to json format.
+        // console.log(data)
+        setItems(data); //empty array filled
       } catch (err) {
         console.log(err);
       }
     };
 
-    fetchItems();
-  }, [reqType]);
+    fetchItems(); //invoke function
+  }, [reqType]); // /posts /users /comments
 
   return (
     <div className="App">
