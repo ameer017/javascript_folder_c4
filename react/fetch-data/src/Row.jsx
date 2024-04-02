29e28ac91769594cd63fc6f5c1784def634792ba
ameer017 <rajiabdullahi907@gmail.com>
2024-04-02 22:24:53 +0100

    project-done

--- react/fetch-data/src/Row.jsx
+++ react/fetch-data/src/Row.jsx
@@ -1,15 +1,17 @@
 import Cell from "./Cell";
 
 const Row = ({ item }) => {
-    return (
-        <tr>
-            {Object.entries(item).map(([key, value]) => {
-                return (
-                    <Cell key={key} cellData={JSON.stringify(value)} />
-                )
-            })}
-        </tr>
-    )
-}
+  return (
+    <tr>
+      {Object.entries(item).map(([key, value]) => {
+        return <Cell key={key} cellData={JSON.stringify(value)} />;
+      })}
+    </tr>
+  );
+};
 
-export default Row
+export default Row;
+
+// Object.entries(item) is used to convert the properties of the item object into an array of [key, value] pairs. This array is then mapped over using .map(), where each key-value pair is processed to create a Cell component.
+
+// Object.entries() is a method in JavaScript that returns an array of a given object's string-keyed property [key, value] pairs, 
