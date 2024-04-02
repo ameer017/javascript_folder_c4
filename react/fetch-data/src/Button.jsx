93d44258f986b57147e9628099043de7b58973b1
ameer017 <rajiabdullahi907@gmail.com>
2024-04-02 21:54:21 +0100

    optimization

--- react/fetch-data/src/Button.jsx
+++ react/fetch-data/src/Button.jsx
@@ -1,13 +1,17 @@
 const Button = ({ buttonText, reqType, setReqType }) => {
-    return (
-        <button
-            className={buttonText === reqType ? "selected" : null}
-            type="button"
-            onClick={() => setReqType(buttonText)}
-        >
-            {buttonText}
-        </button>
-    )
-}
+  return (
+    <button
+      className={buttonText === reqType ? "selected" : null}
+      type="button"
+      onClick={() => setReqType(buttonText)} //end point set to the button Text
+    >
+      {buttonText}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
+
+// buttonText: This prop represents the text content of the button.
+// reqType: This prop represents the currently selected request type.
+// setReqType: This prop is a function used to update the selected request type.
--- react/fetch-data/src/Form.jsx
+++ react/fetch-data/src/Form.jsx
@@ -1,26 +1,16 @@
-import Button from "./Button"
-
+import Button from "./Button";
 
 const Form = ({ reqType, setReqType }) => {
-    return (
-        <form onSubmit={(e) => e.preventDefault()}>
-            <Button
-                buttonText="users"
-                reqType={reqType}
-                setReqType={setReqType}
-            />
-            <Button
-                buttonText="posts"
-                reqType={reqType}
-                setReqType={setReqType}
-            />
-            <Button
-                buttonText="comments"
-                reqType={reqType}
-                setReqType={setReqType}
-            />
-        </form>
-    )
-}
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <Button buttonText="users" reqType={reqType} setReqType={setReqType} />
+      <Button buttonText="posts" reqType={reqType} setReqType={setReqType} />
+      <Button buttonText="comments" reqType={reqType} setReqType={setReqType} />
+    </form>
+  );
+};
 
-export default Form
+export default Form;
+
+// Inside the form, it renders three instances of the Button component with different buttonText props ("users", "posts", "comments").
+// Each Button component is passed the reqType and setReqType props, allowing them to interact with the state managed by the parent component.
