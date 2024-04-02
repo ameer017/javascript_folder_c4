8e36d2d5572b052ab459ab19d0a0f366094e13a8
ameer017 <rajiabdullahi907@gmail.com>
2024-04-02 15:58:11 +0100

    route-configured

--- react/react-router/src/App.jsx
+++ react/react-router/src/App.jsx
@@ -1,12 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import Header from "./Header";
+import Footer from "./Footer";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
