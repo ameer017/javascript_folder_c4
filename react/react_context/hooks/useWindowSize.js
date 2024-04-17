fccde69c2ad5ef52dd96ca292ffeab212dc34685
ameer017 <rajiabdullahi907@gmail.com>
2024-04-17 21:55:12 +0100

    context-api

---
+++ react/react_context/hooks/useWindowSize.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // const cleanUp = () => {
+    //   console.log("runs if a useEffect deps changes");
+    //   window.removeEventListener("resize", handleResize);
+    // };
+
+    return () => window.removeEventListener("resize", handleResize);;
+    // return cleanUp;
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
