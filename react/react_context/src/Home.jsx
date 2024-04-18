ab66c43de4ae2fbab31772fdc93e53374ba8bb43
ameer017 <rajiabdullahi907@gmail.com>
2024-04-18 07:32:49 +0100

    complete

--- react/react_context/src/Home.jsx
+++ react/react_context/src/Home.jsx
@@ -3,7 +3,7 @@ import Feed from "./Feed";
 import DataContext from "../context/DataContext";
 
 const Home = () => {
-  const { posts, fetchError, isLoading } = useContext(DataContext);
+  const { searchResults, fetchError, isLoading } = useContext(DataContext);
   return (
     <main className="Home">
       {isLoading && <p className="statusMsg">Loading posts...</p>}
@@ -14,8 +14,8 @@ const Home = () => {
       )}
       {!isLoading &&
         !fetchError &&
-        (posts.length ? (
-          <Feed posts={posts} />
+        (searchResults.length ? (
+          <Feed posts={searchResults} />
         ) : (
           <p className="statusMsg">No posts to display.</p>
         ))}
